4c5b0dc848462124c6f6eafa99c54764a7d76007
yangm11 <mingzhang.yang@nih.gov>
2017-03-30 12:26:15 -0400

    gobang updates

--- src/goBang.js
+++ src/goBang.js
@@ -4,26 +4,26 @@
 'use strict';
 
 // set the counter
-var count = 0;
+let count = 0;
 
 // initiate the board data object
-var board;
+let board;
 
 // initiate whiteList, blackList, and trashBin as array
-var whiteList = [];
-var blackList = [];
-var allClicked = [];
-var trashBin = [];
+let whiteList = [];
+let blackList = [];
+let allClicked = [];
+let trashBin = [];
 
 // create the board object of 19 x 19
 function createBoardObject(n) {
-  var i, j;
-  var board = {};
-  var coor;
+  let model = {};
+  let coor;
+  let i, j;
   for (i = 0; i < n; i++) {
     for (j = 0; j < n; j++) {
       coor = 'x' + i + 'y' + j;
-      board[coor] = {
+      model[coor] = {
         x: i,
         y: j,
         clicked: false,
@@ -32,13 +32,13 @@ function createBoardObject(n) {
       }
     }
   }
-  return board;
+  return model;
 }
 
 // create the board svg of 19 x 19
 function createBoard(n) {
-  var i, j;
-  var text = `<svg height="${n * 30}" width="${n * 30}">`;
+  let i, j;
+  let text = `<svg height="${n * 30}" width="${n * 30}">`;
   for (i = 0; i < n; i++) {
     text += `<line x1="${i * 30 + 15}" y1="15" x2="${i * 30 + 15}" 
 y2="${n * 30 - 15}" style="stroke:rgb(0,0,0);stroke-width:2" />`;
@@ -46,8 +46,8 @@ y2="${n * 30 - 15}" style="stroke:rgb(0,0,0);stroke-width:2" />`;
   }
   for (i = 0; i < n; i++) {
     for (j = 0; j < n; j++) {
-      var x = i * 30 + 15;
-      var y = j * 30 + 15;
+      let x = i * 30 + 15;
+      let y = j * 30 + 15;
       text += `<g id="x${i}y${j}">
 <circle  cx="${x}" cy="${y}" r="15" stroke="black" stroke-opacity="0" />
 <g class="marker">
@@ -65,7 +65,7 @@ y2="${n * 30 - 15}" style="stroke:rgb(0,0,0);stroke-width:2" />`;
 }
 
 function click(elem) {
-  var point = board[elem.id];
+  let point = board[elem.id];
   if (point.clicked) {
     return;
   }
@@ -74,10 +74,10 @@ function click(elem) {
     d3.select(`#${elem.id}`).attr('class', 'black');
     point.clicked = true;
     count += 1;
-    point.mark = count;
+    point.mark = count + '';
     point.class = 'black';
     d3.select(`#text-${elem.id}`).text(count + '');
-    var g = elem.getElementsByTagName('g')[0];
+    let g = elem.getElementsByTagName('g')[0];
     elem.removeChild(g);
     blackList.push(elem.id);
     allClicked.push(elem.id);
@@ -87,9 +87,9 @@ function click(elem) {
     point.clicked = true;
     count += 1;
     point.class = 'white';
-    point.mark = count + "";
+    point.mark = count + '';
     d3.select(`#text-${elem.id}`).text(count + '');
-    var g = elem.getElementsByTagName('g')[0];
+    let g = elem.getElementsByTagName('g')[0];
     elem.removeChild(g);
     whiteList.push(elem.id);
     allClicked.push(elem.id);
@@ -99,8 +99,8 @@ function click(elem) {
 
 function addEL(n) {
   console.log('start');
-  for (var i = 0; i < n; i++) {
-    for (var j = 0; j < n; j++) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
       document.getElementById(`x${i}y${j}`).addEventListener('click', function () {
         console.log(this.id);
         click(this);
@@ -113,18 +113,18 @@ function back() {
   if (allClicked.length === 0) {
     return;
   }
-  var key = allClicked.pop();
+  let key = allClicked.pop();
   console.log(key);
-  var point = board[key];
+  let point = board[key];
   point.class = '';
-  point.mark = count - 1;
+  point.mark = '';
   point.clicked = false;
   d3.select(`#${key}`).attr('class', 'removed');
   d3.select(`#text-${key}`).text('');
 
   count -= 1;
 
-  var toBeRestored = trashBin.pop();
+  let toBeRestored = trashBin.pop();
   document.getElementById(`${key}`).appendChild(toBeRestored);
 
   if (whiteList.slice(-1)[0] === key) {
@@ -133,3 +133,38 @@ function back() {
     blackList.pop();
   }
 }
+
+let dr =  {
+  left: function (elem) {
+    let cr = 'x' + (elem.x - 1) + 'y' + elem.y;
+    return board[cr] ? board[cr] : null;
+  },
+  leftUp: function (elem) {
+    let cr = 'x' + (elem.x - 1) + 'y' + (elem.y - 1);
+    return board[cr] ? board[cr] : null;
+  },
+  up: function (elem) {
+    let cr = 'x' + elem.x + 'y' + (elem.y - 1);
+    return board[cr] ? board[cr] : null;
+  },
+  rightUp: function (elem) {
+    let cr = 'x' + (elem.x + 1) + 'y' + (elem.y - 1);
+    return board[cr] ? board[cr] : null;
+  },
+  right: function (elem) {
+    let cr = 'x' + (elem.x + 1) + 'y' + elem.y;
+    return board[cr] ? board[cr] : null;
+  },
+  rightDown: function (elem) {
+    let cr = 'x' + (elem.x + 1) + 'y' + (elem.y + 1);
+    return board[cr] ? board[cr] : null;
+  },
+  down: function (elem) {
+    let cr = 'x' + elem.x  + 'y' + (elem.y + 1);
+    return board[cr] ? board[cr] : null;
+  },
+  leftDown: function (elem) {
+    let cr = 'x' + (elem.x - 1) + 'y' + (elem.y + 1);
+    return board[cr] ? board[cr] : null;
+  }
+};
