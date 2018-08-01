a659009bf4f87a2a668dac68ba0ae92e502a8019
Mingzhang Yang <mingzhang.yang@nih.gov>
2018-08-01 12:20:46 -0400

    creating image assistant

---
+++ src/imageAssistant.js
@@ -0,0 +1,14 @@
+/**
+ * Created by yangm11 on 8/1/2018.
+ */
+'use strict';
+
+function drawImageOnCanvas() {
+
+}
+
+if (typeof module !== 'undefined' && module.parent) {
+
+} else {
+  // test code go here
+}
